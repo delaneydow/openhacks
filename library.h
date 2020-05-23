7a94d108fe8b0193b08bdcce1e4efa7cb62562a7
Michae17l <mjhowes17@gmail.com>
2020-05-23 14:53:03 -0400

    Update library.h
    test

---
+++ library.h
@@ -0,0 +1,31 @@
+#ifndef lib_h
+#define lib_h
+#include <string>
+
+class Exposure{
+    //Everything returned as a type double, as all this data is to be combined into a point-driven formula to compute a single percentage
+    private:
+    //location will be a string 
+        double location();
+        double outsideHouse(); //will be a double, and will take in both hours and minutes, perhaps seconds
+        double interactions(); //asking the user how many interactions he/she has had
+        double precautions(); //asking the user about mask wearing, time spent social distancing
+        double livingConditions(); //asking the user where he she lives
+    public:
+        double caseNumber(); //taken from google/ open source info
+        double spreadRate(); //taken from google 
+};
+class Danger{
+    private:
+        double age(); //will return the users age in format : You are x years and y months old
+        double gender();  
+        double medicalConditions(); //asks the user about previous medical conditions, returns input
+        
+};      
+
+
+
+
+
+
+#endif 
